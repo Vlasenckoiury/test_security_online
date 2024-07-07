04b9f9603ad893c423a2a1b88e531d8b16a947db
iuryvlasenko <vlasencko.iury1999@yandex.by>
2024-07-07 23:09:51 +0300

    Реализовано что могут только заказчики добавлять задачи

--- task/users/permissions.py
+++ task/users/permissions.py
@@ -6,3 +6,8 @@ class IsCustomerOrAssignedEmployee(permissions.BasePermission):
         if request.user.user_type == 'admin':
             return True
         return obj.customer == request.user or obj.employee == request.user
+
+
+class IsCustomer(permissions.BasePermission):  # Разрешает только заказчикам
+    def has_permission(self, request, view):
+        return request.user and request.user.user_type == 'customer'
--- task/users/views.py
+++ task/users/views.py
@@ -1,11 +1,12 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
 from . import models
 from .serializers import CustomTokenObtainPairSerializer
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from .models import Task
 from .serializers import TaskSerializer
+from rest_framework.exceptions import PermissionDenied
 
 
 class CustomTokenObtainPairView(TokenObtainPairView):
@@ -40,8 +41,26 @@ class TaskViewSet(viewsets.ModelViewSet):
             return Task.objects.all()
         elif user.user_type == 'employee':
             return Task.objects.filter(models.Q(employee=user) | models.Q(customer=user))
+        elif user.user_type == 'customer':
+            return Task.objects.filter(customer=user)
         else:
             return Task.objects.filter(customer=user)
 
     def perform_create(self, serializer):
-        serializer.save(customer=self.request.user)
+        user = self.request.user
+        if user.user_type != 'customer':
+            raise PermissionDenied("Только заказчики могут создавать задачи.")
+        serializer.save(customer=user)
+
+    def create(self, request, *args, **kwargs):
+        if request.user.user_type != 'customer':
+            return Response({'detail': 'Только заказчики могут создавать задачи.'}, status=status.HTTP_403_FORBIDDEN)
+        return super().create(request, *args, **kwargs)
+
+    # def perform_create(self, serializer):
+    #     serializer.save(customer=self.request.user)
+
+    def get_permissions(self):
+        if self.action in ['create']:
+            return [IsCustomer()]
+        return [permissions.IsAuthenticated()]
