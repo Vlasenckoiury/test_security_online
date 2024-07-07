bf00342b7e93e0ed9f0058bb5d0c50efea90b12e
iuryvlasenko <vlasencko.iury1999@yandex.by>
2024-07-07 09:39:31 +0300

    настроены урлы

---
+++ task/users/urls.py
@@ -0,0 +1,8 @@
+# users/urls.py
+from django.urls import path
+from .views import LoginAPIView, LogoutAPIView
+
+urlpatterns = [
+    path('login/', LoginAPIView.as_view(), name='login'),
+    path('logout/', LogoutAPIView.as_view(), name='logout'),
+]
